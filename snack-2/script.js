75c2ffeaba31b9fc6bb0c876d417c032f2480901
BelliaNino <bellianino1996@gmail.com>
2026-02-20 15:09:58 +0100

    add snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,14 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+//metodo for()
+for (let i = 0; i < people.length; i++){
+  const firstname = people[i].name
+  console.log(firstname);
+}
+
+//metodo forEach()
+people.forEach(firstname => (console.log(firstname.name)))
+
+
