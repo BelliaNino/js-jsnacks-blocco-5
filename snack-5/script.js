39e6c1039cf479295a72b7056240dfc5fe06f7bd
BelliaNino <bellianino1996@gmail.com>
2026-02-20 15:54:24 +0100

    add snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,16 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+let even = []
+
+for (let i = 0; i < nums.length; i++){
+    const num = nums[i]
+    if(num % 2 == 0){
+        even.push(num)
+    }}
+
+console.log(even);
+
+const evenNum = nums.filter(num => (num % 2 == 0));
+console.log(evenNum);
