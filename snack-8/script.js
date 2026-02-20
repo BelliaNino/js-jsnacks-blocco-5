771b971eeb8f83257325e225fac57b6779cffcdd
BelliaNino <bellianino1996@gmail.com>
2026-02-20 17:03:11 +0100

    add snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,24 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+//Metodo for()
+/*for (let i = 0; i < students.length; i++){
+  const student = students[0].class
+  console.log(student);
+  break 
+}*/
+
+for (let i = 0; i < students.length; i++){
+  const student = students[i]
+  if (student.name === 'Marco Lanci'){
+    const studentClass = student.class
+    console.log(studentClass); 
+  }
+}
+
+
+//Metodo find()
+const clasStudent = students.find(student => (student.name === 'Marco Lanci'))
+console.log(clasStudent.class);
+
